486c4b40ac0e6fd2f11ee213ba9bf1c4932a16be
bodroghykristof <bodroghykristof@gmail.com>
2020-12-11 15:59:00 +0100

    Create and autowire RemoteURLReader into PokemonController

--- src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
+++ src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
@@ -1,5 +1,6 @@
 package com.codecool.reactomonspringboot.controller;
 
+import com.codecool.reactomonspringboot.controller.service.RemoteURLReader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PokemonController {
 
 	private static final String DEFAULT_NUMBER_OF_ITEMS_PER_PAGE = "20";
+	private final RemoteURLReader urlReader;
+
+	public PokemonController(RemoteURLReader urlReader) {
+		this.urlReader = urlReader;
+	}
 
 	@GetMapping("/pokemon")
 	public String fetchPokemon(@RequestParam(defaultValue = "0") int offset, @RequestParam(defaultValue = DEFAULT_NUMBER_OF_ITEMS_PER_PAGE) int limit) {
