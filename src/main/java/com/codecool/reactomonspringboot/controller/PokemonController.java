6f2be87028572c2dc675c4472b3c19d5d65d995f
bodroghykristof <bodroghykristof@gmail.com>
2020-12-11 15:53:53 +0100

    Implement accessing query parameters with default values

--- src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
+++ src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
@@ -1,14 +1,19 @@
 package com.codecool.reactomonspringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PokemonController {
 
+	private static final String DEFAULT_NUMBER_OF_ITEMS_PER_PAGE = "20";
+
 	@GetMapping("/pokemon")
-	public String fetchPokemon() {
-		return ("Hey, this is your first sample Pokemon");
+	public String fetchPokemon(@RequestParam(defaultValue = "0") int offset, @RequestParam(defaultValue = DEFAULT_NUMBER_OF_ITEMS_PER_PAGE) int limit) {
+//		System.out.println(offset);
+		System.out.println(limit + 50);
+		return ("Hey, this is your first sample Pokemon for offset " + offset + " and limit " + limit);
 	}
 
 }
