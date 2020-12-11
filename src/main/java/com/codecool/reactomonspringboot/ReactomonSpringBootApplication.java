ea0f9b36543245613dc3ebaa362e763c4ef3ea99
bodroghykristof <bodroghykristof@gmail.com>
2020-12-11 15:30:54 +0100

    Starter commit

---
+++ src/main/java/com/codecool/reactomonspringboot/ReactomonSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.reactomonspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactomonSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactomonSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/reactomonspringboot/ReactomonSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.reactomonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactomonSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
