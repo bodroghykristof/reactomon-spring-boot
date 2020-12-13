b31637d708b0e3f4f1cf83536ded8105ccf5d3c3
bodroghykristof <bodroghykristof@gmail.com>
2020-12-13 18:04:21 +0100

    Remove @CrossOrigin annotations

--- src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
+++ src/main/java/com/codecool/reactomonspringboot/controller/PokemonController.java
@@ -34,7 +34,6 @@ public class PokemonController {
 	}
 
 	@GetMapping("/pokemon/{id}")
-	@CrossOrigin
 	public String fetchPokemonById(@PathVariable int id, HttpServletResponse response) {
 		try {
 			String pokemonBaseData = urlReader.readFromUrl(POKEMON_API_ROOT + "pokemon/" + id);
@@ -50,7 +49,6 @@ public class PokemonController {
 	}
 
 	@GetMapping("/type")
-	@CrossOrigin
 	public String fetchTypes(HttpServletResponse response) {
 		try {
 			return urlReader.readFromUrl(POKEMON_API_ROOT + "type");
@@ -61,7 +59,6 @@ public class PokemonController {
 	}
 
 	@GetMapping("/pokemon/image/{id}")
-	@CrossOrigin
 	public String fetchImageForPokemon(@PathVariable int id) {
 		JSONObject imageData = new JSONObject();
 		imageData.put("image", POKEMON_IMAGE_ROOT + id + ".png");
